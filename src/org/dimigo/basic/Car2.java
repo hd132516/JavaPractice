c700d1762d2889c29e1d16c48afb4aa5984ea2d0
Vista <retail3210@gmail.com>
2015-04-16 12:18:30 +0900

    실습과제 7-2번

---
+++ src/org/dimigo/basic/Car2.java
@@ -0,0 +1,66 @@
+package org.dimigo.basic;
+
+/**
+ * Car - Abstraction of the car
+ * Copyright (C) 2015 Therne. All rights are reserved.
+ *
+ * @author Vista
+ */
+public class Car2 {
+    private String company;
+    private String model;
+    private String color;
+    private int maxSpeed;
+    private int price;
+
+    public Car2() {
+    }
+
+    public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, int newPrice) {
+        company = newCompany;
+        model = newModel;
+        color = newColor;
+        maxSpeed = newMaxSpeed;
+        price = newPrice;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
