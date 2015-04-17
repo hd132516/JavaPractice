a43148f279ba61ba2f191ac0c97dd7039023e6b7
Vista <retail3210@gmail.com>
2015-04-17 13:55:19 +0900

    실습과제 7-3번 : Constructor Overloading (수정)

--- JavaPractice/src/org/dimigo/oop/Car3.java
+++ JavaPractice/src/org/dimigo/oop/Car3.java
@@ -8,6 +8,10 @@ package org.dimigo.oop;
  */
 public class Car3 extends Car {
 
+    public Car3() {
+
+    }
+
     public Car3(String company, String model, String color, int maxSpeed, int price) {
         this.company = company;
         this.model = model;
@@ -15,4 +19,18 @@ public class Car3 extends Car {
         this.maxSpeed = maxSpeed;
         this.price = price;
     }
+
+    public Car3(String company, String model, String color, int maxSpeed) {
+        this.company = company;
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Car3(String company, String model, String color) {
+        this.company = company;
+        this.model = model;
+        this.color = color;
+    }
+
 }
