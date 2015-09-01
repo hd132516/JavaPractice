5574456f6ab64ab8459823a8b732a0713479b30d
Vista <retail3210@gmail.com>
2015-09-01 10:33:39 +0900

    [실습과제 3] 스마트폰 업그레이드

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,24 @@
+package org.dimigo.abstractclass;
+
+/**
+ * Copyright (C) 2015 Therne. All rights are reserved.
+ *
+ * @author Vista
+ */
+public class SmartPhoneTest {
+    public static void main(String[] args) {
+        IPhone iPhone = new IPhone("iPhone 6", "애플", 700000);
+        System.out.println(iPhone);
+        iPhone.turnOn();
+        iPhone.pay();
+        iPhone.useSpecialFunction();
+        iPhone.turnOff();
+
+        Galaxy galaxy = new Galaxy("갤럭시 S6", "삼성", 650000);
+        System.out.println(galaxy);
+        galaxy.turnOn();
+        galaxy.pay();
+        galaxy.useSpecialFunction();
+        galaxy.turnOff();
+    }
+}
